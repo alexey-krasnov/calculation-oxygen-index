ed154815a687e383c7f05ce9ad2c713ab17412e4
Aleksei Krasnov <alexeykrasnov1989@gmail.com>
2022-06-22 17:38:15 +0300

    Initial version

--- oxygen_index.py
+++ oxygen_index.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 # -*- coding: UTF-8 -*-
 import pandas as pd
 import chemparse
@@ -12,8 +18,13 @@ def parse_formula(formula: str) -> dict:
     """Parse semiconductor formula and return chemical composition as dictionary {'element': index, etc.}"""
     return chemparse.parse_formula(formula)
 
+
+# In[12]:
+
+
 charge_state = {
         "O": -2,
+        "Li": 1,
         "Na": 1,
         "Mg": 2,
         "Al": 3,
@@ -52,26 +63,50 @@ charge_state = {
         "Tm": 3,
         "Yb": 3,
         "Bi": 3,
+        "Ga": 3,        
                 }
 
 
+# In[54]:
+
+
 if __name__ == "__main__":
-        df = pd.read_excel('to_calculate.xlsx')
-        pd.options.display.max_columns = 12
+    df = pd.read_excel('to_calculate.xlsx')
+    pd.options.display.max_columns = 12
+
+    for semiconductor in df['Initial composition']:
+        positive_charge = 0
+        new_composition = ''
+        formula_as_dict = parse_formula(semiconductor)
+        for el, ind in formula_as_dict.items():
+            if el != 'O':
+                positive_charge += ind*charge_state[el]
+                new_composition += f'{el}{ind}'
+        new_composition = new_composition + 'O' + f'{round(positive_charge/2, 3)}'
+        print(new_composition, sep='\n')
+        # df['Revised composition'] = new_composition
+    print(df.head(30))
 
 
-        for semiconductor in df['Initial composition']:
-                # print(semiconductor)
-                formula_as_dict = parse_formula(semiconductor)
-                # print(formula_as_dict)
-                for el, ind in formula_as_dict.items():
-                    print(el, ind)
+# In[43]:
+
+
+semiconductor = get_formula()
+formula_as_dict = parse_formula(semiconductor)
+print(formula_as_dict)
+positive_charge = 0
+new_composition = ''
+for el, ind in formula_as_dict.items():
+    if el != 'O':
+        positive_charge += ind*charge_state[el]
+        new_composition += f'{el}{ind}'
+new_composition = new_composition + 'O' + f'{round(positive_charge/2, 3)}'
+
+print(new_composition)
+
+
+# In[ ]:
 
 
 
 
-        # semiconductor = get_formula()
-        # formula_as_dict = parse_formula(semiconductor)
-        #
-        # for el, ind in formula_as_dict.items():
-        #     print(el, ind)
